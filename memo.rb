bc09b44c2e54352db6d9af632a4a33029c6c2edc
EC2 Default User <ec2-user@ip-172-31-35-233.us-east-2.compute.internal>
2021-07-09 02:52:53 +0000

    2nd commit

--- memo.rb
+++ memo.rb
@@ -1,26 +1,26 @@
 require "csv"
 
-puts "「 あ 」＝新規でメモを作成　「 い 」＝既存のメモを編集する"
+puts "1(新規でメモを作成する）　2(既存のメモを編集する)"
 
-memo_type = gets.chomp
+memo_type = gets.to_i
 
-if memo_type == "あ"
+if memo_type == 1
   puts "拡張子を除いたファイル名を入力"
   file_name = gets.chomp
   file= File.open("#{file_name}.csv","w")
   
   puts "メモを入力"
-  puts "入力したら、「ENTER」で保存"
-  memo = gets
+  puts "入力したら、ctl+Dで保存"
+  memo = STDIN.read
   file.puts "#{memo}"
   
-elsif memo_type == "い"
+elsif memo_type == 2
   puts "拡張子を除いたファイル名を入力"
   file_name = gets.chomp
   file = File.open("#{file_name}.csv","a")
   puts "メモを入力"
-  puts "入力したら、「ENTER」で保存"
-  memo = gets
+  puts "入力したら、ctl+Dで保存"
+  memo = STDIN.read
   file.puts "#{memo}"
   
   
